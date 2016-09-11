f0bdf92b87301e755d8832e17f903da405be1a17
csf30816 <elijahporter@zoho.com>
2016-09-11 17:11:39 -0500

    Update api.js

--- api.js
+++ api.js
@@ -3,4 +3,4 @@ var API = {};
 API.PROJECT_URL = 'projects.scratch.mit.edu';
 API.CLOUD_URL = 'cloud.scratch.mit.edu';
 API.CDN_URL = 'cdn.scratch.mit.edu';
-API.CLOUD_URL.PORT = '531';
+API.CLOUD_PORT = '531';
