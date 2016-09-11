981a492034ca052c95b509d5b03c2c6c8a0a2fe7
csf30816 <elijahporter@zoho.com>
2016-09-11 17:28:39 -0500

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,21 @@
 var API = {};
 
+var SERVER = 'scratch.mit.edu';
 API.PROJECT_URL = 'projects.scratch.mit.edu';
 API.CLOUD_URL = 'cloud.scratch.mit.edu';
 API.CDN_URL = 'cdn.scratch.mit.edu';
 API.CLOUD_PORT = '531';
+
+API.SESSION_FILE = '.scratchSession';
+
+var Scratch = {};
+
+Scratch.projectJSON = function(ID) {
+  $.ajax({
+    type: "GET",
+    url: API.PROJECT_URL + '/internalapi/project/' + id + "/get/",
+    success: function (responseText) {
+      return(responseText);
+    }
+  })
+}
